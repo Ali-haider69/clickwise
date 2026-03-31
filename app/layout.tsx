c9e190195eec9554137c6fd463c04ef39ed1e518
Ali-haider69 <haiderrafiq69@gmail.com>
2026-03-31 09:21:12 +0500

    script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4721856353237082"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QGRPNT8SRQ" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">{`
           window.dataLayer = window.dataLayer || [];
